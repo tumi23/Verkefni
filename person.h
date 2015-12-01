ac041da575995d9730bf8fa7796a0bddd682de1d
Steinar Snær Guðjónsson <steinar@cock.li>
2015-12-01 22:21:13 +0000

    cleanup person header

--- person.h
+++ person.h
@@ -1,14 +1,15 @@
-#ifndef PERSON_H
-#define PERSON_H
 #include <iostream>
-#include <sstream>
-#include <string>
 #include <fstream>
 #include <string>
 #include <vector>
 using namespace std;
-class Person
-{
+
+class Person {
+    string fname;
+    string lname;
+    string sex;
+    string birth;
+    string death;
 public:
     Person(string firstname, string lastname, string tsex, string tbirth, string tdeath);
     void testwriter();
@@ -17,12 +18,4 @@ public:
     string getsex();
     string getbirth();
     string getdeath();
-private:
-    string fname;
-    string lname;
-    string sex;
-    string birth;
-    string death;
 };
-
-#endif // PERSON_H
