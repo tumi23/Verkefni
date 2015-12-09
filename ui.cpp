9ff18984a8423651746c0bac5ecf84ff9d64f7fe
unknown <steinar@cock.li>
2015-12-09 02:30:43 +0000

    ui BIG cleanup

--- ui.cpp
+++ ui.cpp
@@ -1,63 +1,45 @@
 #include "ui.h"
 #include <algorithm>
 
-void UI::UIDbChoseLooper(vector<computer>& Comp, vector<Person>& Per, Data& d, PersonWorkLayer& pom, computerWorkLayer& com)
-{
-    int input;
-    bool breaker = false;
-    d.dbStarter();
-    d.personLoad(Per);
-    d.computerLoad(Comp);
-    while(breaker == false)
-    {
-        cout << "(1)Person Database\n(2)Computer Database\n!Anything Else Quits!" << endl;
-        cin >> input;
-        switch(input){
-            case 1:
-                personStarter(Per,Comp,d,pom);
-                break;
-            case 2:
-                computerStarter(Comp,d,com);
-                break;
-            default:
-                breaker = true;
-        }
-    }
-}
-
-void UI::personStarter(vector<Person>& Per,vector<computer>& Comp, Data& d, PersonWorkLayer& pom)
-{
-    bool breaker = false;
-    cout << "\nThis is a Database to register\nand view Famous Computer people\n" << endl;
-    while(breaker == false)
-    {
-        breaker = personChooser(Per,Comp,d,pom);
-        d.personLoad(Per);
-    }
-}
-
-int UI::GetPersonChoice(int srtornot)//Gives information on what the user wants to do
-{
-    int input;
-    if(srtornot == 0)
-        cout << "\n(1)Add Person\n(2)Display Persons\n(3)Search by name\n(4)Sort\n(5)Connect Computer and Person\n!Anything else returns you back to choose a database!\n" << endl;
-    else if(srtornot == 1)
-        cout << "Sort by:\n(1)First name\n(2)Last name\n(3)Sex\n(4)Date of birth\n(5)Date of death" << endl;
+char UI::getInput() {
+    char input = 0;
     cin >> input;
     return input;
 }
 
-bool UI::personChooser(vector<Person>& Per,vector<computer>& Comp, Data& d,PersonWorkLayer& pom)
-{
-    int srtornot = 0;
-    bool breaker = false;
-    string pId, comname;
-    int input = GetPersonChoice(srtornot);
-    switch (input) {
-        case 1:
-            pom.insert(d,Per,Comp);
-            break;
-        case 2:
+void UI::UIDbChoseLooper(vector<computer>& Comp, vector<Person>& Per, Data& d, PersonWorkLayer& pom, computerWorkLayer& com) {
+    d.dbStarter();
+    d.personLoad(Per);
+    d.computerLoad(Comp);
+
+    while(true) {
+        cout << "(1)Person Database - (2)Computer Database\n!Anything Else Quits!" << endl;
+        switch (getInput()) {
+            case '1': while(personChooser(Per,Comp,d,pom)); break;
+            case '2': while(computerChooser(Comp,d,com)); break;
+            default: return;
+        }
+    }
+}
+
+//Gives information on what the user wants to do
+void UI::printMenu(string menu) {
+    switch (menu) {
+        case "personMain": cout << "\n(1)Add Person - (2)Display Persons - (3)Search by name\n(4)Sort - (5)Connect Computer and Person\n!Anything else returns you back to choose a database!\n" << endl; break;
+        case "personSort": cout << "Sort by:\n(1)First name\n(2)Last name\n(3)Sex\n(4)Date of birth\n(5)Date of death" << endl; break;
+        case "computerMain": cout << "\n(1)Add Computer\n(2)Display computer\n(3)Search by name\n(4)sort\n!Anything else returns you back to choose a database!\n" << endl; break;
+        case "computerSort": cout << "Sort by:\n(1)Name\n(2)Year made\n(3)Type\n(4)If it was made or not\n" << endl; break;
+        default: cout << "Error menu not found!" << endl;
+    }
+}       
+
+bool UI::personChooser(vector<Person>& Per,vector<computer>& Comp, Data& d,PersonWorkLayer& pom) {
+    string pId, comname;\d.
+    personLoad(Per);
+    printMenu("personMain");
+    switch (getInput()) {
+        case '1': pom.insert(d,Per,Comp); break;
+        case '2':
             for(size_t i = 0; i < Per.size(); i++)
             {
                 cout << Per[i];
@@ -66,73 +48,31 @@ bool UI::personChooser(vector<Person>& Per,vector<computer>& Comp, Data& d,Perso
                 cout << "Computer Created: " << comname << endl;
             }
             break;
-        case 3:
-            pom.searchByName(Per);
-            break;
-        case 4:
-            srtornot = 1;
-            input = GetPersonChoice(srtornot);
-            switch(input){
-            case 1:
-                pom.sortName(Per);
-                break;
-            case 2:
-                pom.sortLastName(Per);
-                break;
-            case 3:
-                pom.sortSex(Per);
-                break;
-            case 4:
-                pom.sortBirth(Per);
-                break;
-            case 5:
-                pom.sortDeath(Per);
-                break;
-            default:
-                cout << "Wrong input!" << endl;
+        case '3': pom.searchByName(Per); break;
+        case '4':
+            printMenu("personSort");
+            switch(getInput()) {
+                case '1': pom.sortName(Per); break;
+                case '2': pom.sortLastName(Per); break;
+                case '3': pom.sortSex(Per); break;
+                case '4': pom.sortBirth(Per); break;
+                case '5': pom.sortDeath(Per); break;
+                default: cout << "Error wrong input!" << endl;
             }
-        case 5:
-            pom.modifyConnection(d,Per,Comp);
             break;
-        default:
-            breaker = true;
-        }
-  return breaker;
-}
-
-void UI::computerStarter(vector<computer>& Comp, Data& d,computerWorkLayer& com)
-{
-    bool breaker = false;
-    cout << "\nThis is a Database to register\nand view Famous computer\n" << endl;
-    while(breaker == false)
-    {
-        breaker = computerChooser(Comp,d,com);
-        d.computerLoad(Comp);
+        case '5': pom.modifyConnection(d,Per,Comp); break;
+        default: return false;
     }
+    return true;
 }
 
-int UI::GetComputerChoice(int srtornot)
-{
-    int input;
-    if(srtornot == 0)
-        cout << "\n(1)Add Computer\n(2)Display computer\n(3)Search by name\n(4)sort\n!Anything else returns you back to choose a database!\n" << endl;
-    else if(srtornot == 1)
-        cout << "Sort by:\n(1)Name\n(2)Year made\n(3)Type\n(4)If it was made or not\n" << endl;
-    cin >> input;
-    return input;
-}
-
-bool UI::computerChooser(vector<computer>& Comp, Data& d,computerWorkLayer& com)
-{
-    int srtornot = 0;
-    bool breaker = false;
+bool UI::computerChooser(vector<computer>& Comp, Data& d,computerWorkLayer& com) {
+    d.computerLoad(Comp);
     string cId, name;
-    int input = GetComputerChoice(srtornot);
-    switch (input) {
-        case 1:
-            com.insert(Comp, d);
-            break;
-        case 2:
+    printMenu("computerMain");
+    switch (getInput()) {
+        case '1': com.insert(Comp, d); break;
+        case '2':
             for(size_t i = 0; i < Comp.size(); i++)
             {
                 cout << Comp[i];
@@ -141,34 +81,18 @@ bool UI::computerChooser(vector<computer>& Comp, Data& d,computerWorkLayer& com)
                 cout << "Creator: " << name << endl;
             }
             break;
-        case 3:
-            com.searchByName(Comp,d);
-            break;
-        case 4:
-            srtornot = 1;
-            input = GetComputerChoice(srtornot);
-            switch(input){
-            case 1:
-                com.sortName(Comp,d);
-                break;
-            case 2:
-                cout << "Sort By Year Made" << endl;
-                com.sortYearMade(Comp,d);
-                break;
-            case 3:
-                cout << "Sort By Type" << endl;
-                com.sortType(Comp,d);
-                break;
-            case 4:
-                cout << "Sort By if it has been made or not" << endl;
-                com.sortMade(Comp,d);
-                break;
-            default:
-                cout << "Wrong input!" << endl;
+        case '3': com.searchByName(Comp,d); break;
+        case '4':
+            printMenu("computerSort");
+            switch(getInput()){
+                case '1': com.sortName(Comp,d); break;
+                case '2': com.sortYearMade(Comp,d); break;
+                case '3': com.sortType(Comp,d); break;
+                case '4': com.sortMade(Comp,d); break;
+                default: cout << "Error wrong input!" << endl;
             }
             break;
-        default:
-            breaker = true;
-  }
-  return breaker;
+        default: return false;
+    }
+    return true;
 }
--- ui.h
+++ ui.h
@@ -14,12 +14,10 @@ using namespace std;
 
 struct UI {
     void UIDbChoseLooper(vector<computer>& Comp, vector<Person>& Per, Data& d, PersonWorkLayer& pom, computerWorkLayer& com);
-    void personStarter(vector<Person>& Per,vector<computer>& Comp, Data& d,PersonWorkLayer& pom);
     bool personChooser(vector<Person>& Per,vector<computer>& Comp, Data& d,PersonWorkLayer& pom);
-    int GetPersonChoice(int srtornot);
-    void computerStarter(vector<computer>& Comp, Data& d,computerWorkLayer& com);
     bool computerChooser(vector<computer>& Comp, Data& d,computerWorkLayer& com);
-    int GetComputerChoice(int srtornot);
+    void printMenu(string menu);
+    char getInput();
 };
 
 #endif // UI_H
